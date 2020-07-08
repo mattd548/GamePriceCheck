9dd932504a3aa904601d6513562e7bc2d98c6d9f
Matthew Delaguila <matthewd548@gmail.com>
2020-07-08 03:54:26 -0400

    added scanner object for user input, validated input using loop,and closed resource

--- GamePriceChecker.java
+++ GamePriceChecker.java
@@ -1,5 +1,6 @@
 import java.io.IOException;
 import java.net.MalformedURLException;
+import java.util.Scanner;
 
 import com.gargoylesoftware.htmlunit.FailingHttpStatusCodeException;
 
@@ -8,25 +9,45 @@ public class GamePriceChecker {
 	
 
 	public static void main(String[] args) throws FailingHttpStatusCodeException, MalformedURLException, IOException 
-	{
-		String game_title="the outerworlds ps4";
+{       String game_title;
+		boolean isSet= false;
+		System.out.println("Please Enter the video game you are looking for and we will check the prices");
+		//closes resource scanner
+		try(Scanner input = new Scanner(System.in)){
+			
+			// while nothing is entered it prompts the user to enter again
+	 while(isSet==false) 
+	 {
+		
+	
+		game_title= input.nextLine();
+		if(game_title.isEmpty())
+		{
+	      System.out.println("No video game title was entered, please try again");
+	      isSet=false;
+	     
+	    }
+	        
+		
+		else {
+		isSet=true;
 		System.out.println("BestBuy");
 		checkBestBuy(game_title);
+		System.out.println();
 		System.out.println("Amazon");
 		checkAmazon(game_title);
-	/*	private BestBuyScraping bestbuy_scraper= new BestBuyScraping();
-		//t.getPageAstext();
-		bestbuy_scraper.setForm();
-		bestbuy_scraper.setSearchButton();
-		bestbuy_scraper.inputSearch(game_title);
-		bestbuy_scraper.selectTitle();
-		//t.getPageAstext();
-		bestbuy_scraper.selectPrice();
-		bestbuy_scraper.selectPublisher();
-        //System.out.println(game.getString_new_price());
-		*/
+		}
 		
-	}
+	 }
+	
+		}
+	
+
+
+
+
+
+}
 	
 	
 	public static void checkBestBuy(String game_title) throws FailingHttpStatusCodeException, MalformedURLException, IOException
